875b728b90fb06b00d44551ab4b7f7931b852d8f
hyerimsn <etcheef@likelion.org>
2020-07-15 22:30:07 +0900

    [완료] 회원가입과 로그인 기능

---
+++ LittleAchievement/account/models.py
@@ -0,0 +1,7 @@
+from django.db import models
+from django.conf import settings
+# Create your models here.
+
+# class Profile(models.Model):
+#     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+#     username = models.CharField('별명', max_length=10)
