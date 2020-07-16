f80850ad6407c580a453807c26f0139893e153df
chichi <jang.chihun@gmail.com>
2020-07-16 11:51:17 +0900

    [add] index page를 이용한 구독 버튼 동작 Test

--- LittleAchievement/task/views.py
+++ LittleAchievement/task/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from .models import CommonTask,MyTask
 # Create your views here.
+def subscribe(request):
+    
+    target_task = CommonTask.objects.get(id=request.POST.get('subscribe_task'))
+    
+    print(target_task)
+    MyTask.objects.create(user= request.user, task = target_task)
+    
+    return redirect('index')
