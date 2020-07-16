b7193cbc47bd9d3825329bbebf1fb0cb644ed7d1
chichi <jang.chihun@gmail.com>
2020-07-16 11:50:48 +0900

    [mdf] model structure rebuilding

--- LittleAchievement/task/admin.py
+++ LittleAchievement/task/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import CommonTask,CustomTask,MyTask
+from .models import CommonTask,MyTask
 # Register your models here.
 admin.site.register(CommonTask)
-admin.site.register(CustomTask)
 admin.site.register(MyTask)
--- LittleAchievement/task/models.py
+++ LittleAchievement/task/models.py
@@ -3,18 +3,19 @@ from django.conf import settings
 from django.utils import timezone
 
 # Create your models here.
+
 class CommonTask(models.Model):
+    maker = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.SET_DEFAULT , default ="전학간 친구에요")  #탈퇴한 사용자꺼 보존
     name = models.CharField("할 일", max_length=100)
     created = models.DateTimeField(editable=False, default=timezone.now)
 
     def __str__(self):
         return self.name    
-class CustomTask(CommonTask):
-    maker = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.SET_DEFAULT , default ="전학간 친구에요")  #탈퇴한 사용자꺼 보존하자
 
-   
-class MyTask(CustomTask):
+class MyTask(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE )
+    task = models.ForeignKey(CommonTask,on_delete=models.CASCADE )  #탈퇴한 사용자꺼 보존
 
-    
+    def __str__(self):
+        return str(self.task) 
 
