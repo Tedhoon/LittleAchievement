39aee6da7095873b6a40a7a730e0167bb0eb4f6f
chichi <jang.chihun@gmail.com>
2020-07-16 11:50:08 +0900

    [add] url 분리

--- LittleAchievement/config/urls.py
+++ LittleAchievement/config/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from django.contrib.auth import views as auth_views
 from account.views import index, signup
 
@@ -23,5 +23,6 @@ urlpatterns = [
     path('', index, name="index"),
     path('signup/', signup, name="signup"),
     path('login/', auth_views.LoginView.as_view(), name="login"),
-    path('logout/', auth_views.LogoutView.as_view(), name="logout")
+    path('logout/', auth_views.LogoutView.as_view(), name="logout"),
+    path('task/', include('task.urls')),
 ]
